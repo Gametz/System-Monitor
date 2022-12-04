373f9ff264bfb70dc7987dc0892027ac4c08a8f9
Vllad <40424029+Gametz@users.noreply.github.com>
2022-12-05 02:01:26 +0300

    1.1

--- Practic/Properties/AssemblyInfo.cs
+++ Practic/Properties/AssemblyInfo.cs
@@ -1,15 +1,16 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
 // связанных со сборкой.
-[assembly: AssemblyTitle("Practic")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("System Monitor")]
+[assembly: AssemblyDescription("Программа для просмотра информации о системе")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Practic")]
+[assembly: AssemblyProduct("System Monitor")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +33,6 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: NeutralResourcesLanguage("ru")]
